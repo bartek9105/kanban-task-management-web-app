53816f99b641797ebc1ea19265f644b79c8fa7e3
Bartłomiej Matoga <bartlomiej.matoga@miquido.com>
2023-12-13 20:52:10 +0100

    feat: introduce dnd library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import Navbar from "./components/Navbar";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styles from "./App.module.scss";
+
+const ITEMS = Array.from({ length: 10 }, (v, k) => k).map((k) => ({
+  id: `item-${k}`,
+  content: `item ${k}`,
+}));
+
+console.log(ITEMS);
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className={styles.container}>
+        <DragDropContext onDragEnd={() => {}}>
+          <Droppable droppableId="droppable">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {ITEMS.map((item, index) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={`${item.id}`}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {item.content}
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
   );
 }
